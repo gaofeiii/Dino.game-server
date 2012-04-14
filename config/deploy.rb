f6feaec47172db26f411bebbed49230177f50ee3
magic0fei <magic0fei@gmail.com>
2012-04-14 15:53:04 +0800

    add deploy and unicorn file

---
+++ config/deploy.rb
@@ -0,0 +1,81 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+require 'bundler/capistrano'
+
+# Server list
+@test = "vm-192-168-14-216.shengyun.grandcloud.cn"
+
+# Deploy server
+@@server = :test
+
+set :rvm_ruby_string, "1.9.3@rails323"
+set :rvm_type, :user
+
+set :bundle_dir, '$HOME/.rvm/gems/ruby-1.9.3-p125@rails323'
+
+default_run_options[:pty] = true
+set :user, "gaofei"
+set :runner, "gaofei"
+set :ssh_options,   { :forward_agent => true }
+set :application, "dinosaur"
+set :deploy_to, "/var/games/servers/#{application}"
+# set :deploy_via, :remote_cache
+set :rails_env, :production
+set :use_sudo, false
+set :keep_releases, 5
+
+set :repository,  "git@vm-192-168-14-216.shengyun.grandcloud.cn:/git/dinosaur.git"
+set :scm, :git
+set :branch, "master"
+set :scm_passphrase, "123"
+
+role :web, eval("@#{@@server}")
+role :app, eval("@#{@@server}")
+role :db,  eval("@#{@@server}"), :primary => true # This is where Rails migrations will run
+
+namespace :deploy do
+  %w(start stop restart).each do |action|
+    desc "unicorn:#{action}"
+    task action.to_sym do
+      find_and_execute_task("unicorn:#{action}")
+    end
+  end
+ 
+end
+
+namespace :assets do
+  desc "assets:precompile"
+  task :precompile, :role => :app do
+    run "cd #{current_path} && bundle exec rake assets:precompile"
+  end
+end
+
+namespace :rvm do
+  task :trust_rvmrc do
+    run "rvm rvmrc trust \#\{release_path\}"
+  end
+end
+
+# 如果有rvmrc文件需要执行 trust_rvmrc
+# after "deploy", "rvm:trust_rvmrc"
+after "deploy", "deploy:migrate"
+# after "deploy:create_symlink", "assets:precompile"
+
+namespace :unicorn do
+  desc "Start unicorn"
+    task :start, :roles => :app do
+      run "cd #{current_path} && bundle exec unicorn -c #{current_path}/config/unicorn.rb -D -E production"
+    end
+
+    desc "Stop unicorn"
+    task :stop, :roles => :app do
+      run "kill -QUIT `cat #{current_path}/tmp/pids/unicorn.pid`"
+      sleep(3)
+    end
+
+    desc "Restart unicorn"
+    task :restart, :roles => :app do
+      find_and_execute_task("unicorn:stop")
+      find_and_execute_task("unicorn:start")
+    end
+end
---
+++ config/unicorn.rb
@@ -0,0 +1,43 @@
+worker_processes 1
+
+working_directory "/var/games/servers/dinosaur/current"
+
+listen "/tmp/accounts.sock", :backlog => 128
+
+preload_app true
+timeout 30
+pid "/var/games/servers/dinosaur/shared/pids/unicorn.pid"
+stderr_path "/var/games/servers/dinosaur/shared/log/unicorn.stderr.log"
+stdout_path "/var/games/servers/dinosaur/shared/log/unicorn.stdout.log"
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Before forking, kill the master process that belongs to the .oldbin PID.
+  # This enables 0 downtime deploys.
+  old_pid = "/var/games/servers/dinosaur/shared/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  # the following is *required* for Rails + "preload_app true",
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+
+  # if preload_app is true, then you may also want to check and
+  # restart any other shared sockets/descriptors such as Memcached,
+  # and Redis.  TokyoCabinet file handles are safe to reuse
+  # between any number of forked children (assuming your kernel
+  # correctly implements pread()/pwrite() system calls)
+end
