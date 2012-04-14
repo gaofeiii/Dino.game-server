a8c3bd3f43e88f85275a8733921a2c2bacbcb49b
magic0fei <magic0fei@gmail.com>
2012-04-14 16:27:42 +0800

    change villages controller json structure

--- app/controllers/villages_controller.rb
+++ app/controllers/villages_controller.rb
@@ -1,8 +1,7 @@
 class VillagesController < ApplicationController
 
 	def index
-		p Ohm.redis.info
 		player = Player[params[:player_id]]
-		render :json => player.try(:village)
+		render :json => {:village => player.try(:village)}
 	end
 end
