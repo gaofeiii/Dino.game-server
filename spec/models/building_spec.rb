0d753b06ba0ea2d2bfdd33d1e457d492ad8dba6e
gaofei <magic0fei@gmail.com>
2012-04-19 13:49:01 +0800

    add spec

--- spec/models/building_spec.rb
+++ spec/models/building_spec.rb
@@ -11,5 +11,11 @@ describe Building do
 		it "should respond to village" do
 			@building.should respond_to(:village)
 		end
+
+		it "should get the correct village" do
+			village = FactoryGirl.create(:village)
+			building_b = FactoryGirl.create(:building, :village_id => village.id)
+			building_b.village.should == village
+		end
 	end
 end
