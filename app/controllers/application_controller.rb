b2a119b6133dc5d9e07595a0d5849a70490481e0
gaofei <magic0fei@gmail.com>
2012-04-20 10:51:09 +0800

    remove filter for controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,9 +16,9 @@ class ApplicationController < ActionController::Base
   end
 
   def find_player
-  	@current_player = Session.find_by_session_key(params[:session_key]).try(:player)
-  	unless @current_player
-  		render :json => "Login expired.", :status => 998 and return
-  	end
+  	# @current_player = Session.find_by_session_key(params[:session_key]).try(:player)
+  	# unless @current_player
+  	# 	render :json => "Login expired.", :status => 998 and return
+  	# end
   end
 end
