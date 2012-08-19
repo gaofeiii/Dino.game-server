7ec425fd40a0ddaa5c4ebc63fb832296e3efd553
magic0fei <magic0fei@gmail.com>
2012-08-20 02:51:45 +0800

    Fix error

--- app/controllers/real_time_info_controller.rb
+++ app/controllers/real_time_info_controller.rb
@@ -3,10 +3,6 @@ class RealTimeInfoController < ApplicationController
 	before_filter :validate_player
 
 	def refresh
-		world_chat = []
-		data = {
-			:world_chat_messages => world_chat
-		}
-		render :json = data
+		render :json => []
 	end
 end
