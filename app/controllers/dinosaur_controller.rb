5132ae38f60afdb026610fd29c92708a371ffe12
magic0fei <magic0fei@gmail.com>
2012-11-01 03:27:56 +0800

    Add message when dinosaur is full

--- app/controllers/dinosaur_controller.rb
+++ app/controllers/dinosaur_controller.rb
@@ -13,7 +13,12 @@ class DinosaurController < ApplicationController
 	end
 
 	def feed
-		food = @player.specialties.find(:type => params[:food_type].to_i).first
+		food = @player.foods.find(:type => params[:food_type].to_i).first
+
+		if (@dinosaur.hunger_time - @dinosaur.feed_point) < 10
+			render :json => {:error => "BABY_IS_FULL"} and return
+		end
+
 		if food.nil? || food.count <= 0
 			render :json => {:error => "NOT_ENOUGH_FOOD"} and return
 		else
