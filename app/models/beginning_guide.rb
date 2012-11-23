97d6588ee4db286e959a26c88b442840711cc86f
magic0fei <magic0fei@gmail.com>
2012-11-24 04:56:50 +0800

    Change some guide method

--- app/models/beginning_guide.rb
+++ app/models/beginning_guide.rb
@@ -1,5 +1,32 @@
 # Only included by player model.
 # Require ohm, ohm-contrib gems.
+
+## === Example ===
+# 
+# player = Player.sample
+# player.guide_info
+# => {}
+#
+# player.guide_info.current_quest
+# => {:index=>1, :finished=>0, :rewarded=>0}
+#
+# player.guide_info.current_quest.finished?
+# => false
+#
+# player.guide_info.current_quest.finished = true
+# => true
+#
+# player.guide_info.current_quest
+# => {:index=>1, :finished=>1, :rewarded=>0}
+#
+# player.guide_info.current_quest.rewarded = true
+# => true
+#
+# player.guide_info[1]
+# => {:index=>1, :finished=>1, :rewarded=>1}
+#
+# player.guide_info.current_quest
+# => {:index=>2, :finished=>0, :rewarded=>0}
 module BeginningGuide
 	def self.included(model)
 		model.attribute :guide_info
@@ -14,15 +41,70 @@ module BeginningGuide
 	end
 
 	def guide_info
-		@attributes[:guide_info] = @attributes[:guide_info].kind_of?(Hash) ? @attributes[:guide_info] : JSON.parse(@attributes[:guide_info] || "{}")
-		@attributes[:guide_info].keys.each do |key|
-			@attributes[:guide_info][(key.to_i rescue key) || key] = @attributes[:guide_info].delete(key).extend(BeginningGuideHelper)
+		if @attributes[:guide_info].kind_of?(Hash)
+			return @attributes[:guide_info]
+		else
+			@attributes[:guide_info] = @attributes[:guide_info].nil? ? {} : JSON.parse(@attributes[:guide_info])
+			@attributes[:guide_info].keys.each do |key|
+				@attributes[:guide_info][(key.to_i rescue key) || key] = @attributes[:guide_info].delete(key).symbolize_keys!.extend(BeginningGuideSingleHelper)
+			end
+			@attributes[:guide_info].extend(BeginningGuideHelper)
+			return @attributes[:guide_info]
 		end
-		@attributes[:guide_info]
 	end
 	
 end
 
 module BeginningGuideHelper
-	
+	def [](index)
+		v = super || {:index => index, :finished => 0, :rewarded => 0}.extend(BeginningGuideSingleHelper)
+		if super.blank? && index > 0
+			self[index] = v
+		else
+			v
+		end
+	end
+
+	def current_quest
+		if self.blank?
+			self[1]
+		else
+			i = keys.max
+			if self[i].finished? && self[i].rewarded?
+				i += 1
+			end
+			return self[i]
+		end
+	end
+
+	def next_quest
+		i = keys.max || 0
+		self[i + 1]
+	end
+
 end
+
+module BeginningGuideSingleHelper
+	%w(finished rewarded).each do |name|
+		ind = name == "finished" ? 0 : 1
+
+		define_method("#{name}?") do
+			self[name.to_sym] == 1 ? true : false
+		end
+
+		define_method("#{name}=") do |sig|
+			self[name.to_sym] = (sig ? 1 : 0)
+		end
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
