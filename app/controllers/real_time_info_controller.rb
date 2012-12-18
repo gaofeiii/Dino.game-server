6197608c6660fd4f75aa65e9a721fa2e38fa7895
magic0fei <magic0fei@gmail.com>
2012-12-18 21:33:32 +0800

    x

--- app/controllers/real_time_info_controller.rb
+++ app/controllers/real_time_info_controller.rb
@@ -3,13 +3,13 @@ class RealTimeInfoController < ApplicationController
 	before_filter :validate_player, :only => [:info]
 
 	def info
-		chats = ChatMessage.world_message(params[:world_chat_last_id], 10)
-			+ ChatMessage.league_message(params[:league_id], params[:league_chat_last_id], 10)
-			+ ChatMessage.private_message(@player.id, params[:private_chat_last_id], 10)
-
 		data = {
-			:chats => chats
+			:data => {
+				:buildings => Building.cost,
+				:technologies => Technology.cost
+			}
 		}
+			
 		render_sucess(data)
 	end
 end
