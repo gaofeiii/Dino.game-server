22d3912c8ad98deafc582acb08e1bb0a8d211bcb
magic0fei <magic0fei@gmail.com>
2013-02-05 16:30:42 +0800

    update background cronjob

--- app/models/background.rb
+++ app/models/background.rb
@@ -1,5 +1,6 @@
 class Background
 	@@queue = []
+	@@cronjob = []
 
 	# Background.add_queue(Troops, 1, "refresh!", 1360045836)
 	def self.add_queue(klass, id, action, time)
@@ -18,10 +19,6 @@ class Background
 		@@queue
 	end
 
-	def self.add_cronjob(klass, id, action, time)
-		
-	end
-
 	def self.refresh_queues
 		self.all_queues.each do |queue_key|
 			key_arr = queue_key.split(':')
@@ -33,8 +30,62 @@ class Background
 			ids = Ohm.redis.zrangebyscore queue_key, "-inf", Time.now.to_i
 			ids.each do |k_id|
 				obj = klass[k_id]
-				next if obj.nil?
+				if obj.nil?
+					Ohm.redis.zrem queue_key, k_id
+					next
+				end
 				obj.send(action)
+				Ohm.redis.zrem queue_key, k_id
+			end
+		end
+	end
+
+	Cronjob = Struct.new(:duration, :last_exec_time) do
+		def to_hash
+			{
+				:duration => duration,
+				:last_exec_time => last_exec_time
+			}
+		end
+
+		def to_map
+			to_hash.to_a.flatten
+		end
+	end
+
+	def self.add_cronjob(klass, action, time_duration)
+		key = klass.key[:cronjob][action]
+		@@cronjob << key
+		Ohm.redis.multi do |t|
+			t.sadd "Background:cronjobs", key
+			t.hmset key, 'time_duration', time_duration
+		end
+	end
+
+	def self.all_cronjobs
+		if @@cronjob.empty?
+			@@cronjob = Ohm.redis.smembers "Background:cronjobs"
+		end
+		@@cronjob
+	end	
+
+	def self.refresh_cronjobs
+		self.all_cronjobs.map do |cron_key|
+			cron = Ohm.redis.hgetall cron_key # keys = [:time_duration, :last_exec_time]
+			if cron.empty?
+				Ohm.redis.del cron_key
+				next
+			end
+
+			now_time = ::Time.now.to_i
+			if cron['last_exec_time'].to_i + cron['time_duration'].to_i <= now_time
+				key_arr = cron_key.split(':')
+				klass = key_arr.first.constantize
+				action = key_arr.last
+				
+				raise "Invalid cronjob class:#{klass} @#{__FILE__}:#{__LINE__}" if klass.nil?
+				klass.send(action)
+				Ohm.redis.hset cron_key, 'last_exec_time', now_time
 			end
 		end
 	end
