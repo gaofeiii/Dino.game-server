f682df4cff9c77f997a539676ada7dc8c1ecafdc
magic0fei <magic0fei@gmail.com>
2013-02-26 05:28:18 +0800

    x

--- config/server_info.rb
+++ config/server_info.rb
@@ -1,5 +1,15 @@
 require 'socket'
 
+class Hash
+	def deep_symbolize_keys
+    inject({}) { |result, (key, value)|
+      value = value.deep_symbolize_keys if value.is_a?(Hash)
+      result[(key.to_sym rescue key) || key] = value
+      result
+    }
+  end unless Hash.method_defined?(:deep_symbolize_keys)
+end
+
 class ServerInfo
 	# include OhmExtension
 
