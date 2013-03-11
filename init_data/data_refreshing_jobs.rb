a517c8e50ddaec6663723f763783fb6f6d79abff
magic0fei <magic0fei@gmail.com>
2013-03-11 15:24:04 +0800

    update cronjob

--- init_data/data_refreshing_jobs.rb
+++ init_data/data_refreshing_jobs.rb
@@ -7,3 +7,5 @@ puts "--- Add cronjobs ---"
 Background.add_cronjob(WorldChat, 'clean_up!', 2.hours.to_i)
 Background.add_cronjob(LeagueChat, 'clean_up!', 2.hours.to_i)
 Background.add_cronjob(PrivateChat, 'clean_up!', 1.day.to_i)
+Background.add_cronjob(Player, 'update_all_battle_rank!', 1.hour.to_i)
+Background.add_cronjob(League, 'update_all_battle_rank!', 1.hour.to_i)
