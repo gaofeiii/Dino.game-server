f906a9bffa9b658f5a9763a324df071ef8774976
magic0fei <magic0fei@gmail.com>
2013-04-09 21:39:31 +0800

    x

--- app/models/player_ios_helper.rb
+++ app/models/player_ios_helper.rb
@@ -5,6 +5,8 @@ module PlayerIosHelper
 	
 	module InstanceMethods
 		def send_push(message)
+			return if device_token.blank?
+			
 			send_push_message(:device_token => device_token, :message => message)
 		end
 
