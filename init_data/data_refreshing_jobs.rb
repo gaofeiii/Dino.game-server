88c5ba99935398861a5d7da78ac98dec0c3d6aa4
magic0fei <magic0fei@gmail.com>
2013-04-30 13:18:47 +0800

    x

--- init_data/data_refreshing_jobs.rb
+++ init_data/data_refreshing_jobs.rb
@@ -8,7 +8,7 @@ Ohm.redis.set('Server:status', 1)
 puts "--- Adding system queues ---"
 
 puts "--- Adding cronjobs ---"
-# Background.clear_all_cronjobs
+Background.clear_all_cronjobs
 # # Background.add_cronjob(WorldChat, 'clean_up!', 2.hours.to_i)
 # # Background.add_cronjob(LeagueChat, 'clean_up!', 2.hours.to_i)
 # # Background.add_cronjob(PrivateChat, 'clean_up!', 1.day.to_i)
@@ -19,5 +19,4 @@ LeagueWar.start!
 Background.add_cronjob(Stat, 'record_all', 1.hour)
 Background.add_cronjob(Mail, 'clean_up!', 6.hours)
 Background.add_cronjob(Deal, 'clean_up!', 1.hour)
-Background.add_cronjob(GoldMine, 'refresh_all_players_goldmine', 1.hour)
 Background.add_cronjob(AdvisorRelation, 'clean_up!', 1.hour)
