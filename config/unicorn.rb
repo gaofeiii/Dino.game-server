90e8f9238f09c6b04a7645ebdd9e6231ade88499
magic0fei <magic0fei@gmail.com>
2013-05-21 10:48:39 +0800

    x

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,23 +10,23 @@ current_path = "/var/games/servers/#{application}/current"
 require "#{current_path}/config/server_info.rb"
 
 proc_count = case RbConfig::CONFIG['host_os']
-  when /darwin9/
-    `hwprefs cpu_count`.to_i
-  when /darwin/
-    ((`which hwprefs` != '') ? `hwprefs thread_count` : `sysctl -n hw.ncpu`).to_i
-  when /linux/
-    `cat /proc/cpuinfo | grep processor | wc -l`.to_i
-  when /freebsd/
-    `sysctl -n hw.ncpu`.to_i
-  when /mswin|mingw/
-    require 'win32ole'
-    wmi = WIN32OLE.connect("winmgmts://")
-    cpu = wmi.ExecQuery("select NumberOfCores from Win32_Processor") # TODO count hyper-threaded in this
-    cpu.to_enum.first.NumberOfCores
-  else
-    1
-  end
+when /darwin9/
+  `hwprefs cpu_count`.to_i
+when /darwin/
+  ((`which hwprefs` != '') ? `hwprefs thread_count` : `sysctl -n hw.ncpu`).to_i
+when /linux/
+  `cat /proc/cpuinfo | grep processor | wc -l`.to_i
+when /freebsd/
+  `sysctl -n hw.ncpu`.to_i
+when /mswin|mingw/
+  require 'win32ole'
+  wmi = WIN32OLE.connect("winmgmts://")
+  cpu = wmi.ExecQuery("select NumberOfCores from Win32_Processor") # TODO count hyper-threaded in this
+  cpu.to_enum.first.NumberOfCores
+else
+  1
 end
+
 
 puts "--- Unicorn: the worker_processes is #{proc_count} ==="
 
