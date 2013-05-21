b6649abcaf4243ed5d052a39ad6d4fb852ef68bd
magic0fei <magic0fei@gmail.com>
2013-05-21 10:44:14 +0800

    x

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -9,11 +9,26 @@ current_path = "/var/games/servers/#{application}/current"
 
 require "#{current_path}/config/server_info.rb"
 
-if ServerInfo.info[:env] == "dev"
-  worker_processes 1
-else
-  worker_processes 4
+proc_count = case RbConfig::CONFIG['host_os']
+  when /darwin9/
+    `hwprefs cpu_count`.to_i
+  when /darwin/
+    ((`which hwprefs` != '') ? `hwprefs thread_count` : `sysctl -n hw.ncpu`).to_i
+  when /linux/
+    `cat /proc/cpuinfo | grep processor | wc -l`.to_i
+  when /freebsd/
+    `sysctl -n hw.ncpu`.to_i
+  when /mswin|mingw/
+    require 'win32ole'
+    wmi = WIN32OLE.connect("winmgmts://")
+    cpu = wmi.ExecQuery("select NumberOfCores from Win32_Processor") # TODO count hyper-threaded in this
+    cpu.to_enum.first.NumberOfCores
+  else
+    1
+  end
 end
+
+worker_processes proc_count
 
 listen "/tmp/#{application}.sock", :backlog => 128
 
