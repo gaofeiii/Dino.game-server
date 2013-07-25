298a2d2e1ee53d93aa375625cc9782256816683c
magic0fei <magic0fei@gmail.com>
2013-07-26 02:57:27 +0800

    clean

--- app/models/tool_box.rb
+++ app/models/tool_box.rb
@@ -26,12 +26,13 @@ class ToolBox
 
 		Player.none_npc.each_with_index do |player, i|
 			if player.village.nil? || player.last_login_time < time
-				player.delete
-				cleaned += 1
+				unless player.level < 10 && player.app_store_orders.size == 0
+					player.delete
+					cleaned += 1
+					system('clear') and puts "\e[H\e[2J"
+					puts "--- Cleaned: #{cleaned} | Finished: #{i + 1}/#{count} (#{format("%.2f", (i+1)/count.to_f * 100)}%) ---"
+				end
 			end
-
-			system('clear') and puts "\e[H\e[2J"
-			puts "--- Cleaned: #{cleaned} | Finished: #{i + 1}/#{count} (#{format("%.2f", (i+1)/count.to_f * 100)}%) ---"
 		end
 
 		Country.first.refresh_used_town_nodes
@@ -61,9 +62,17 @@ class ToolBox
 
 	def self.clean_monsters(count = 1000)
 		clean_with_log do
-			count.times do |i|
-				Monster.first.delete
-				log_in_loop("Clean Monsters", i+1, count, i+1)
+			# count.times do |i|
+			# 	Monster.first.delete
+			# 	log_in_loop("Clean Monsters", i+1, count, i+1)
+			# end
+
+			key_array = Ohm.redis.srandmembers(Monster.all.key)
+			real_count = key_array.count
+
+			key_array.each_with_index do |id, index|
+				Monster[id].try(:delete)
+				log_in_loop("Clean Monsters", index+1, real_count, index+1)
 			end
 		end
 	end
